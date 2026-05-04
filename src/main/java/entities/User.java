f244ec611d5f369f4e2e4ec1b301bde3196d0115
Guilherme Franklin <gui.franklin17@gmail.com>
2026-05-04 08:59:57 -0300

    first commit

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,35 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User implements Serializable {
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
