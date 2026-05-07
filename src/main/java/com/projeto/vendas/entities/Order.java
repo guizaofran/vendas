6d37c1653038a7950604cbc0b4f833fc44934658
Guilherme Franklin <gui.franklin17@gmail.com>
2026-05-07 16:54:43 -0300

    OrderStatus

--- src/main/java/com/projeto/vendas/entities/Order.java
+++ src/main/java/com/projeto/vendas/entities/Order.java
@@ -1,35 +1,48 @@
 package com.projeto.vendas.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projeto.vendas.entities.enums.OrderStatus;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
+    @Getter
     private Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
+    @Setter
+    @Getter
     private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
-    private User client;    
+    @Setter
+    @Getter
+    private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -41,5 +54,15 @@ public class Order implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus!=null){
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
     }
 }
